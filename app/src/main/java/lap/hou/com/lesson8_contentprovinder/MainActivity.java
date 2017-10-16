9972c23a90e50a821b38adc4d14f7618a7d9e285
phamlap228 <phamlap228@gmail.com>
2017-10-16 18:01:47 +0700

    content provider

--- app/src/main/java/lap/hou/com/lesson8_contentprovinder/MainActivity.java
+++ app/src/main/java/lap/hou/com/lesson8_contentprovinder/MainActivity.java
@@ -1,7 +1,16 @@
 package lap.hou.com.lesson8_contentprovinder;
 
 import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.database.Cursor;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
